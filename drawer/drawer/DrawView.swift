6ddfd52020f1e79755251e3a51b072fc32145c77
Vlad <Vladislav.fofanov@frtk.ru>
2015-05-07 00:28:43 +0300

    Add wight and color change for lines.

--- drawer/drawer/DrawView.swift
+++ drawer/drawer/DrawView.swift
@@ -11,7 +11,12 @@ import UIKit
 class DrawView: UIView {
     var lines: [Line] = []
     var lastPoint: CGPoint!
-    var drawColor = UIColor.blackColor()
+    //var drawColor = UIColor.blackColor()
+    var lineWidth: CGFloat = 5
+    
+    var Red: CGFloat = 0.5
+    var Blue: CGFloat = 0.5
+    var Green: CGFloat = 0.5
     
     /*init(coder aDecoder: NSCoder!) {
         super.init(coder: aDecoder)
@@ -23,7 +28,7 @@ class DrawView: UIView {
     
     override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
         if let newPoint = (touches.first as? UITouch)?.locationInView(self) {
-            lines.append(Line(start: lastPoint, end: newPoint, color: drawColor))
+            lines.append(Line(start: lastPoint, end: newPoint, width: lineWidth, red: Red, blue: Blue, green: Green))
             lastPoint = newPoint
             self.setNeedsDisplay()
         }
@@ -32,12 +37,13 @@ class DrawView: UIView {
     override func drawRect(rect: CGRect) {
         var context = UIGraphicsGetCurrentContext()
         CGContextSetLineCap(context, kCGLineCapRound)
-        CGContextSetLineWidth(context, 4)
         for line in lines {
             CGContextBeginPath(context)
             CGContextMoveToPoint(context, line.start.x, line.start.y)
             CGContextAddLineToPoint(context, line.end.x, line.end.y)
-            CGContextSetStrokeColorWithColor(context, line.color.CGColor)
+            CGContextSetRGBStrokeColor(context, line.red,line.green ,line.blue ,1)
+            //CGContextSetStrokeColorWithColor(context, line.color.CGColor)
+            CGContextSetLineWidth(context, line.width)
             CGContextStrokePath(context)
         }
     }
